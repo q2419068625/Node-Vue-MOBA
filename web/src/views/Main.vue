a71a054ffe8cf8bc73fbc51ccb43aa190724309d
q2419068625 <q2419068625@qq.com>
2020-10-08 22:06:46 +0800

    主页框架和顶部菜单

---
+++ web/src/views/Main.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <div class="topbar bg-black py-2 px-3 d-flex ai-center">
+        <img src="../assets/logo.png" alt="logo" height="30">
+        <div class="px-2 flex-1">
+            <div class="text-white">王者荣耀</div>
+            <div class="text-grey-1 fs-xxs">团队成就更多</div>
+        </div>
+        <button class="btn bg-primary" type="button ">立即下载</button>
+    </div>
+
+    <div class="bg-primary pt-3 pb-2">
+        <div class="nav d-flex text-white jc-around pd-1">
+            <div class="nav-item active">
+                <router-link to="/" class="nav-link" tag="div">首页</router-link>
+            </div>
+            <div class="nav-item">
+                <router-link to="/" class="nav-link" tag="div">攻略中心</router-link>
+            </div>
+            <div class="nav-item">
+                <router-link to="/" class="nav-link" tag="div">赛事中心</router-link>
+            </div>
+        </div>
+    </div>
+    <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
